f251b18a2f9763d13ed34321f636b701212ddb9f
sumonDev13 <sumonmondal1530765@gmail.com>
2023-10-08 20:16:05 +0600

    wip script

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { userSignUp ,userLogin,getUserInfo} from '../controller/user-controller.js';
+import { userSignUp ,userLogin} from '../controller/user-controller.js';
 
 
 
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post('/signup',userSignUp);
 router.post('/login',userLogin);
-router.get('/userinfo', getUserInfo);
+
 
 
 
